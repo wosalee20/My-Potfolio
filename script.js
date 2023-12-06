38be7fcbf67f0296c68eddd427f3d1d6705a075a
wosalee20 <140804624+wosalee20@users.noreply.github.com>
2023-12-06 09:19:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function toggleMenu() {
-	const menu = document.querySelector(".menu-links");
-	const icon = document.querySelector(".hamburger-icon");
-	menu.classList.toggle("open");
-	icon.classList.toggle("open");
-}
+document.addEventListener("DOMContentLoaded", () => {
+	function toggleMenu() {
+		const menu = document.querySelector(".menu-links");
+		const icon = document.querySelector(".hamburger-icon");
+		menu.classList.toggle("open");
+		icon.classList.toggle("open");
+	}
+
+	let isDarkMode = false;
+
+	const toggleDarkMode = () => {
+		isDarkMode = !isDarkMode;
+		document.body.classList.toggle("dark-mode", isDarkMode);
+	};
+
+	const darkModeButton = document.querySelector(".switch-btn");
+	darkModeButton.addEventListener("click", toggleDarkMode);
+});
